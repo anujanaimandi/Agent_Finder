92880c3ef51513bba9d8a2c4b8f4be0af8f08fd9
Timaliii <timalijayasinghe@gmail.com>
2025-05-04 12:34:13 +0530

    created agent.java class

---
+++ src/main/java/com/realestate/models/Agent.java
@@ -0,0 +1,93 @@
+package com.realestate.models;
+
+import java.io.Serializable;
+
+public class Agent implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    private String name;
+    private String contact;
+    private String location;
+    private String specialization;
+    private double rating;
+
+    public Agent(int id, String name, String contact, String location, String specialization, double rating) {
+        this.id = id;
+        this.name = name;
+        this.contact = contact;
+        this.location = location;
+        this.specialization = specialization;
+        this.rating = rating;
+    }
+
+    // Getters and Setters
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+
+        this.id = id;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public String getContact() {
+
+        return contact;
+    }
+
+    public void setContact(String contact) {
+
+        this.contact = contact;
+    }
+
+    public String getLocation() {
+
+        return location;
+    }
+
+    public void setLocation(String location) {
+
+        this.location = location;
+    }
+
+    public String getSpecialization() {
+
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+
+        this.specialization = specialization;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Agent{" +
+                "name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", specialization='" + specialization + '\'' +
+                ", rating=" + rating +
+                '}';
+    }
+}
